822c19d3ac6cc0b937c7b80b7dfdbb31211459c8
kaede.sato <main@kaedesato.work>
2025-06-26 23:15:37 +0900

    Update dependencies in package.json and package-lock.json; replace @node-rs/argon2 with oslo for password handling in login functionality.

--- src/routes/demo/lucia/login/+page.server.ts
+++ src/routes/demo/lucia/login/+page.server.ts
@@ -1,4 +1,4 @@
-import { hash, verify } from '@node-rs/argon2';
+import { Scrypt, verify } from 'oslo/password';
 import { encodeBase32LowerCase } from '@oslojs/encoding';
 import { fail, redirect } from '@sveltejs/kit';
 import { eq } from 'drizzle-orm';
@@ -36,12 +36,8 @@ export const actions: Actions = {
 			return fail(400, { message: 'Incorrect username or password' });
 		}
 
-		const validPassword = await verify(existingUser.passwordHash, password, {
-			memoryCost: 19456,
-			timeCost: 2,
-			outputLen: 32,
-			parallelism: 1
-		});
+		const validPassword = await verify(existingUser.passwordHash, password);
+		
 		if (!validPassword) {
 			return fail(400, { message: 'Incorrect username or password' });
 		}
@@ -65,13 +61,7 @@ export const actions: Actions = {
 		}
 
 		const userId = generateUserId();
-		const passwordHash = await hash(password, {
-			// recommended minimum parameters
-			memoryCost: 19456,
-			timeCost: 2,
-			outputLen: 32,
-			parallelism: 1
-		});
+		const passwordHash = await new Scrypt().hash(password);
 
 		try {
 			await db.insert(table.user).values({ id: userId, username, passwordHash });
